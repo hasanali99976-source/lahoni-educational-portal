edd2fe0b993bd7186ed9c82b43b866239318c6fb
hasan taweel <hasan.ali99976@gmail.com>
2026-08-20 01:00:01 +0300

    Load vivid subject worlds after existing themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import "./final-platform-overrides.css";
 import "./subject-artwork.css";
 import "./subject-scenes.css";
 import "./subject-identity-v3.css";
+import "./subject-worlds-v4.css";
 import { Tajawal } from "next/font/google";
 import type { Metadata, Viewport } from "next";
 import PwaRegister from "./pwa-register";
