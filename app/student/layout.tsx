7a5eb9d9f200cd14ad461bc5a2ad4a9e92210ac8
hasan taweel <hasan.ali99976@gmail.com>
2026-08-23 00:42:35 +0300

    إظهار فصل الطالب داخل بوابته

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -1,6 +1,7 @@
 import "./student-v3.css";
 import StudentSecurity from "./student-security";
+import StudentClassGuard from "./student-class-guard";
 
 export default function StudentLayout({ children }: { children: React.ReactNode }) {
-  return <StudentSecurity>{children}</StudentSecurity>;
+  return <StudentSecurity><StudentClassGuard />{children}</StudentSecurity>;
 }
