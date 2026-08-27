4fece49105bd2729dac456014f16571853dd3f90
hasan taweel <hasan.ali99976@gmail.com>
2026-08-27 18:23:44 +0300

    Remove overlapping student DOM observers

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -1,11 +1,5 @@
 import "./student-v3.css";
-import "./student-code-only.css";
-import StudentSecurity from "./student-security";
-import StudentClassGuard from "./student-class-guard";
-import StudentCodeOnlyUI from "./student-code-only-ui";
-import IosCodeSubmitFix from "./ios-code-submit-fix";
-import StudentExitButton from "./student-exit-button";
 
 export default function StudentLayout({ children }: { children: React.ReactNode }) {
-  return <StudentSecurity><StudentClassGuard /><StudentCodeOnlyUI /><IosCodeSubmitFix /><StudentExitButton />{children}</StudentSecurity>;
+  return <>{children}</>;
 }
