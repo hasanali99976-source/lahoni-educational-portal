86180e1d83a6f9c17728291960743700d0806fa3
hasan taweel <hasan.ali99976@gmail.com>
2026-08-27 09:56:20 +0300

    تفعيل زر الدخول باللمس على الآيفون

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -3,7 +3,8 @@ import "./student-code-only.css";
 import StudentSecurity from "./student-security";
 import StudentClassGuard from "./student-class-guard";
 import StudentCodeOnlyUI from "./student-code-only-ui";
+import IosCodeSubmitFix from "./ios-code-submit-fix";
 
 export default function StudentLayout({ children }: { children: React.ReactNode }) {
-  return <StudentSecurity><StudentClassGuard /><StudentCodeOnlyUI />{children}</StudentSecurity>;
+  return <StudentSecurity><StudentClassGuard /><StudentCodeOnlyUI /><IosCodeSubmitFix />{children}</StudentSecurity>;
 }
