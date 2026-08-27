f0eddf34b6be0ebdfbad230482487f4f360d1818
hasan taweel <hasan.ali99976@gmail.com>
2026-08-27 10:01:26 +0300

    إظهار زر الخروج من بوابة الطالب

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -4,7 +4,8 @@ import StudentSecurity from "./student-security";
 import StudentClassGuard from "./student-class-guard";
 import StudentCodeOnlyUI from "./student-code-only-ui";
 import IosCodeSubmitFix from "./ios-code-submit-fix";
+import StudentExitButton from "./student-exit-button";
 
 export default function StudentLayout({ children }: { children: React.ReactNode }) {
-  return <StudentSecurity><StudentClassGuard /><StudentCodeOnlyUI /><IosCodeSubmitFix />{children}</StudentSecurity>;
+  return <StudentSecurity><StudentClassGuard /><StudentCodeOnlyUI /><IosCodeSubmitFix /><StudentExitButton />{children}</StudentSecurity>;
 }
