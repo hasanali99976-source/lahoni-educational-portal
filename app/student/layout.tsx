95b3d1f124d5679205b17b6a9ab8afbc6a906f97
hasan taweel <hasan.ali99976@gmail.com>
2026-08-28 22:07:19 +0300

    ربط حماية دخول الباركود بتخطيط بوابة الطالب

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -1,5 +1,6 @@
 import "./student-v3.css";
+import StudentQrEntryGuard from "./student-qr-entry-guard";
 
 export default function StudentLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return <><StudentQrEntryGuard />{children}</>;
 }
