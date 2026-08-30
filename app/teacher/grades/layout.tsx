ca34e41e72ca8c797943b5cc428aec642ea7d575
hasan taweel <hasan.ali99976@gmail.com>
2026-08-30 15:39:36 +0300

    تفعيل طباعة سجل الدرجات الكامل

--- app/teacher/grades/layout.tsx
+++ app/teacher/grades/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import CentralRosterSync from "../central-roster-sync";
+import GradesPrintEnhancer from "./grades-print-enhancer";
 
 export default function GradesLayout({ children }: { children: ReactNode }) {
-  return <><CentralRosterSync />{children}</>;
+  return <><CentralRosterSync /><GradesPrintEnhancer />{children}</>;
 }
