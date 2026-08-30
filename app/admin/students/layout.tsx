75d1072fdda2b3df1ca56db214fccd6a47eaad39
hasan taweel <hasan.ali99976@gmail.com>
2026-08-30 13:47:36 +0300

    تفعيل إصلاح نافذة تعديل ونقل الطالب

--- app/admin/students/layout.tsx
+++ app/admin/students/layout.tsx
@@ -1,4 +1,5 @@
 import ClassControlPanel from "./class-control-panel";
+import "./admin-student-modal-fix.css";
 
 export default function AdminStudentsLayout({ children }: { children: React.ReactNode }) {
   return <>
