8461da000b291427fe879ca7cec230a50c34df98
hasan taweel <hasan.ali99976@gmail.com>
2026-09-01 15:33:35 +0300

    تفعيل تصميم بوابة الطالب v67

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -3,6 +3,7 @@ import "./student-mobile-complete.css";
 import "./student-knowledge-v63.css";
 import "./student-refine-v64.css";
 import "./student-premium-v66.css";
+import "./student-wow-v67.css";
 import StudentQrLock from "./student-qr-lock";
 
 export default function StudentLayout({ children }: { children: React.ReactNode }) {
