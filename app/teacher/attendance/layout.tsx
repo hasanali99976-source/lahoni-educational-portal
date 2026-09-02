d13a2f50a3d85140961ff23d3665a33db6e63ba4
hasan taweel <hasan.ali99976@gmail.com>
2026-09-02 20:26:48 +0300

    fix: let attendance PDF button use multi-page generator

--- app/teacher/attendance/layout.tsx
+++ app/teacher/attendance/layout.tsx
@@ -1,10 +1,8 @@
 import type { ReactNode } from "react";
 import AttendanceScheduleGuard from "./attendance-schedule-guard";
-import AttendancePrintEnhancer from "./attendance-print-enhancer";
 
 export default function AttendanceLayout({ children }: { children: ReactNode }) {
   return <>
-    <AttendancePrintEnhancer />
     <AttendanceScheduleGuard />
     {children}
   </>;
