8dc5c937e6ea4834ebe4caab33bc48cf06148ea2
hasan taweel <hasan.ali99976@gmail.com>
2026-09-04 23:30:01 +0300

    design-lab: make bulk roster import primary in students tab

--- app/admin/students/layout.tsx
+++ app/admin/students/layout.tsx
@@ -1,8 +1,10 @@
+import BulkRosterImport from "./bulk-roster-import";
 import ClassControlPanel from "./class-control-panel";
 import "./admin-student-modal-fix.css";
 
 export default function AdminStudentsLayout({ children }: { children: React.ReactNode }) {
   return <>
+    <BulkRosterImport />
     {children}
     <ClassControlPanel />
   </>;
