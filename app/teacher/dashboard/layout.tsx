9b2849bbadb829994a1d5cadfaa8bea8e69d792f
hasan taweel <hasan.ali99976@gmail.com>
2026-09-06 10:26:19 +0300

    feat: connect timetable to original daily tasks

--- app/teacher/dashboard/layout.tsx
+++ app/teacher/dashboard/layout.tsx
@@ -1,9 +1,11 @@
 import type { ReactNode } from "react";
 import DashboardClassLinks from "./dashboard-class-links";
+import DashboardDailyTasks from "./dashboard-daily-tasks";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return <>
     <DashboardClassLinks />
+    <DashboardDailyTasks />
     {children}
   </>;
 }
