f04794869f6a5ccf48f9268c89ba5e4942fd4213
hasan taweel <hasan.ali99976@gmail.com>
2026-09-07 12:21:47 +0300

    fix: activate unit deduction display in academic record

---
+++ app/student/academic-record/layout.tsx
@@ -0,0 +1,5 @@
+import SectionDeductionRuntime from "./section-deduction-runtime";
+
+export default function AcademicRecordLayout({ children }: { children: React.ReactNode }) {
+  return <><SectionDeductionRuntime />{children}</>;
+}
