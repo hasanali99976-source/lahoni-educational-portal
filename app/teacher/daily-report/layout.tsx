02ca7523a1c07602df5aa3a75a8397f788e205ee
hasan taweel <hasan.ali99976@gmail.com>
2026-09-09 21:14:49 +0300

    Use redesigned discipline PDF print renderer

--- app/teacher/daily-report/layout.tsx
+++ app/teacher/daily-report/layout.tsx
@@ -1,12 +1,12 @@
 import type { ReactNode } from "react";
 import TimetableAutoSync from "../timetable/timetable-auto-sync";
-import DisciplineDownloadV3 from "./discipline-download-v3";
+import DisciplineDownloadV4 from "./discipline-download-v4";
 import "./discipline-pdf-v500.css";
 
 export default function DailyReportLayout({ children }: { children: ReactNode }) {
   return <>
     <TimetableAutoSync />
-    <DisciplineDownloadV3 />
+    <DisciplineDownloadV4 />
     {children}
   </>;
 }
