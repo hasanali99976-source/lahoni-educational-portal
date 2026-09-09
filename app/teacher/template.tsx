44788d46f9f91c617b890c87175fc9bfad8e38f5
hasan taweel <hasan.ali99976@gmail.com>
2026-09-09 20:58:57 +0300

    Activate teacher subject gate and loading experience

--- app/teacher/template.tsx
+++ app/teacher/template.tsx
@@ -2,8 +2,10 @@
 
 import type { ReactNode } from "react";
 import TeacherV24RuntimeFixes from "./v24-runtime-fixes";
+import TeacherSubjectGate, { TeacherNavigationLoading } from "./teacher-subject-gate";
 import "./teacher-v400.css";
+import "./teacher-ux-v510.css";
 
 export default function TeacherTemplate({ children }: { children: ReactNode }) {
-  return <><TeacherV24RuntimeFixes />{children}</>;
+  return <><TeacherV24RuntimeFixes /><TeacherSubjectGate /><TeacherNavigationLoading />{children}</>;
 }
