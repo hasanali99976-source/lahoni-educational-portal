f8b53ec6d7b7bbddd7d8b583a11af71d834115b6
hasan taweel <hasan.ali99976@gmail.com>
2026-09-09 22:11:23 +0300

    Load student legacy notification cleanup

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -2,6 +2,7 @@ import "./student-report-pdf-v2.css";
 import "./student-v400.css";
 import "./student-academic-v500.css";
 import "./student-academic-v600.css";
+import "./student-notification-cleanup.css";
 import StudentQrLock from "./student-qr-lock";
 import StudentReportSessionGuard from "./student-report-session-guard";
 import StudentReportPdfV2Runtime from "./student-report-pdf-v2-runtime";
