647f6b5baecac38551b983a2d913e93d3203dd9a
hasan taweel <hasan.ali99976@gmail.com>
2026-09-09 19:14:34 +0300

    Enable teacher v400 portal design

--- app/teacher/template.tsx
+++ app/teacher/template.tsx
@@ -2,6 +2,7 @@
 
 import type { ReactNode } from "react";
 import TeacherV24RuntimeFixes from "./v24-runtime-fixes";
+import "./teacher-v400.css";
 
 export default function TeacherTemplate({ children }: { children: ReactNode }) {
   return <><TeacherV24RuntimeFixes />{children}</>;
