31e7be96721a8d4b9ef9956cd36e821207b0dab2
hasan taweel <hasan.ali99976@gmail.com>
2026-09-11 18:01:13 +0300

    style: apply v17 to teacher management

--- app/admin/teachers/layout.tsx
+++ app/admin/teachers/layout.tsx
@@ -1,4 +1,5 @@
 import "../admin-experience-v16.css";
+import "../admin-experience-v17.css";
 
 export default function AdminTeachersLayout({ children }: { children: React.ReactNode }) {
   return children;
