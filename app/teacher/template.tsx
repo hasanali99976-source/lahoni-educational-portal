5ecbdaf9909e12cea78a1fe46d16318f1d03c8a7
hasan taweel <hasan.ali99976@gmail.com>
2026-09-11 21:57:14 +0300

    feat: activate unified academic teacher experience v40

--- app/teacher/template.tsx
+++ app/teacher/template.tsx
@@ -5,8 +5,8 @@ import TeacherV24RuntimeFixes from "./v24-runtime-fixes";
 import TeacherSubjectGate, { TeacherNavigationLoading } from "./teacher-subject-gate";
 import "./print-theme.css";
 import "./teacher-shell-v38.css";
+import "./teacher-academic-v40.css";
 
-// Production deployment trigger for structural teacher V39.
 export default function TeacherTemplate({ children }: { children: ReactNode }) {
   return <><TeacherV24RuntimeFixes /><TeacherSubjectGate /><TeacherNavigationLoading />{children}</>;
 }
