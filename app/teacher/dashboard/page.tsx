f1c4f6a63a1ae6a7e7722adf84447f22e0d6d0bf
hasan taweel <hasan.ali99976@gmail.com>
2026-09-11 21:58:28 +0300

    feat: show real teacher dashboard analytics

--- app/teacher/dashboard/page.tsx
+++ app/teacher/dashboard/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 
 import TeacherDashboardV31 from "./dashboard-v31";
+import TeacherDashboardAnalyticsV40 from "./dashboard-analytics-v40";
 import "./dashboard-v31.css";
+import "./dashboard-analytics-v40.css";
 
-export default TeacherDashboardV31;
+export default function TeacherDashboardPage(){
+  return <>
+    <TeacherDashboardV31 />
+    <TeacherDashboardAnalyticsV40 />
+  </>;
+}
