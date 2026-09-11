8ab8a3c5dbda4d581c6b56c733b29c0b248546a2
hasan taweel <hasan.ali99976@gmail.com>
2026-09-11 20:33:12 +0300

    preview: activate teacher shell v30

--- app/teacher/template.tsx
+++ app/teacher/template.tsx
@@ -5,6 +5,7 @@ import TeacherV24RuntimeFixes from "./v24-runtime-fixes";
 import TeacherSubjectGate, { TeacherNavigationLoading } from "./teacher-subject-gate";
 import "./teacher-platform-v28.css";
 import "./teacher-platform-v29.css";
+import "./teacher-platform-v30.css";
 
 export default function TeacherTemplate({ children }: { children: ReactNode }) {
   return <><TeacherV24RuntimeFixes /><TeacherSubjectGate /><TeacherNavigationLoading />{children}</>;
