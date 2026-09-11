fcf3f613fc9a5a3d875bb2cf2b5e17085d312ad3
hasan taweel <hasan.ali99976@gmail.com>
2026-09-11 18:23:30 +0300

    style: activate teacher command center v21 safely

--- app/teacher/template.tsx
+++ app/teacher/template.tsx
@@ -5,6 +5,7 @@ import TeacherV24RuntimeFixes from "./v24-runtime-fixes";
 import TeacherSubjectGate, { TeacherNavigationLoading } from "./teacher-subject-gate";
 import "./teacher-v400.css";
 import "./teacher-ux-v510.css";
+import "./teacher-command-v21.css";
 
 export default function TeacherTemplate({ children }: { children: ReactNode }) {
   return <><TeacherV24RuntimeFixes /><TeacherSubjectGate /><TeacherNavigationLoading />{children}</>;
