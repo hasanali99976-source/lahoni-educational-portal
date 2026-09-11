40748153fd28dba053311903826d2ac961fe678d
hasan taweel <hasan.ali99976@gmail.com>
2026-09-11 21:17:00 +0300

    preview: activate unified teacher experience v36

--- app/teacher/template.tsx
+++ app/teacher/template.tsx
@@ -6,6 +6,7 @@ import TeacherSubjectGate, { TeacherNavigationLoading } from "./teacher-subject-
 import "./print-theme.css";
 import "./teacher-platform-v34.css";
 import "./teacher-nav-v35.css";
+import "./teacher-experience-v36.css";
 
 export default function TeacherTemplate({ children }: { children: ReactNode }) {
   return <><TeacherV24RuntimeFixes /><TeacherSubjectGate /><TeacherNavigationLoading />{children}</>;
