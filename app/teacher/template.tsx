e61e7439bd43f6cec2254ae18f36e48b2c5c00a4
hasan taweel <hasan.ali99976@gmail.com>
2026-09-11 21:20:50 +0300

    preview: activate transparent teacher experience v37

--- app/teacher/template.tsx
+++ app/teacher/template.tsx
@@ -5,8 +5,8 @@ import TeacherV24RuntimeFixes from "./v24-runtime-fixes";
 import TeacherSubjectGate, { TeacherNavigationLoading } from "./teacher-subject-gate";
 import "./print-theme.css";
 import "./teacher-platform-v34.css";
-import "./teacher-nav-v35.css";
 import "./teacher-experience-v36.css";
+import "./teacher-experience-v37.css";
 
 export default function TeacherTemplate({ children }: { children: ReactNode }) {
   return <><TeacherV24RuntimeFixes /><TeacherSubjectGate /><TeacherNavigationLoading />{children}</>;
