6165e661fc26f979a7d2a256cc7aaa0b4ca3bbc5
hasan taweel <hasan.ali99976@gmail.com>
2026-09-11 17:59:43 +0300

    style: apply v17 to student management

--- app/admin/students/layout.tsx
+++ app/admin/students/layout.tsx
@@ -8,6 +8,7 @@ import "./students-table-v11.css";
 import "../admin-premium-2026.css";
 import "../admin-experience-v15.css";
 import "../admin-experience-v16.css";
+import "../admin-experience-v17.css";
 
 export default async function AdminStudentsLayout({ children }: { children: React.ReactNode }) {
   const session = await requireSession("admin");
