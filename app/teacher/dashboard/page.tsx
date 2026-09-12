f77d3b84edd2fba5c4010a9c9c63ec83ee03de2b
hasan taweel <hasan.ali99976@gmail.com>
2026-09-12 20:15:41 +0300

    feat: surface linked teacher competition on dashboard

--- app/teacher/dashboard/page.tsx
+++ app/teacher/dashboard/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import TeacherDashboardV31 from "./dashboard-v31";
+import TeacherCompetitionProgress from "../competition-progress";
 import "./dashboard-v31.css";
 
 export default function TeacherDashboardPage(){
-  return <TeacherDashboardV31/>;
+  return <>
+    <TeacherDashboardV31/>
+    <div className="teacher-dashboard-race"><TeacherCompetitionProgress/></div>
+  </>;
 }
