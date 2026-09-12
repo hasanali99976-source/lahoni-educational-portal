96db8b0d6b43f157e131082d0f40b8c08cd350df
hasan taweel <hasan.ali99976@gmail.com>
2026-09-12 16:46:25 +0300

    refactor: centralize teacher typography without inline overrides

--- app/teacher/template.tsx
+++ app/teacher/template.tsx
@@ -2,6 +2,7 @@
 
 import type { ReactNode } from "react";
 import TeacherV24RuntimeFixes from "./v24-runtime-fixes";
+import "./teacher-typography-current.css";
 
 export default function TeacherTemplate({ children }: { children: ReactNode }) {
   return <>
