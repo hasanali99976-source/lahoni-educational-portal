f6f9846a665f03a7f52b677c89f4507cf95457bc
hasan taweel <hasan.ali99976@gmail.com>
2026-09-12 18:57:21 +0300

    style: load teacher shell refinement

--- app/teacher/template.tsx
+++ app/teacher/template.tsx
@@ -6,6 +6,7 @@ import FollowUpEnhancerCurrent from "./follow-up/follow-up-enhancer-current";
 import "./teacher-typography-current.css";
 import "./teacher-current-experience.css";
 import "./teacher-avatar-current.css";
+import "./teacher-shell-refinement-v41.css";
 
 export default function TeacherTemplate({ children }: { children: ReactNode }) {
   return <>
