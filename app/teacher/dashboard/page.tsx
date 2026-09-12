d835dfb2fa02e13b88f19a35ac4ef326b29fcaa9
hasan taweel <hasan.ali99976@gmail.com>
2026-09-12 18:56:09 +0300

    refactor: unify teacher dashboard analytics

--- app/teacher/dashboard/page.tsx
+++ app/teacher/dashboard/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 
 import TeacherDashboardV31 from "./dashboard-v31";
-import TeacherDashboardAnalyticsV40 from "./dashboard-analytics-v40";
 import "./dashboard-v31.css";
-import "./dashboard-analytics-v40.css";
 
 export default function TeacherDashboardPage(){
-  return <>
-    <TeacherDashboardV31 />
-    <TeacherDashboardAnalyticsV40 />
-  </>;
+  return <TeacherDashboardV31/>;
 }
