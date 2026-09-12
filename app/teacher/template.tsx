7bcf48916d31df48b898414ca330871c3aaa45d1
hasan taweel <hasan.ali99976@gmail.com>
2026-09-12 09:25:41 +0300

    refactor: use one canonical teacher visual layer

--- app/teacher/template.tsx
+++ app/teacher/template.tsx
@@ -5,9 +5,6 @@ import TeacherV24RuntimeFixes from "./v24-runtime-fixes";
 import TeacherSubjectGate, { TeacherNavigationLoading } from "./teacher-subject-gate";
 import "./print-theme.css";
 import "./teacher-shell-v38.css";
-import "./teacher-academic-v40.css";
-import "./teacher-profile-v40.css";
-import "./teacher-smart-v41.css";
 
 export default function TeacherTemplate({ children }: { children: ReactNode }) {
   return <><TeacherV24RuntimeFixes /><TeacherSubjectGate /><TeacherNavigationLoading />{children}</>;
