d8951718223e6ff5d3c498a434b40b672d63583f
hasan taweel <hasan.ali99976@gmail.com>
2026-09-13 14:40:06 +0300

    fix: stop expensive competition reads on teacher dashboard

--- app/teacher/dashboard/page.tsx
+++ app/teacher/dashboard/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import TeacherDashboardV31 from "./dashboard-v31";
-import TeacherCompetitionProgress from "../competition-progress";
 import "./dashboard-v31.css";
 
 export default function TeacherDashboardPage(){
-  return <>
-    <TeacherDashboardV31/>
-    <div className="teacher-dashboard-race"><TeacherCompetitionProgress/></div>
-  </>;
+  return <TeacherDashboardV31/>;
 }
