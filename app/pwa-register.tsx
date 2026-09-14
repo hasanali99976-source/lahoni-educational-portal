8b527c289acab10b0fa274c3b62f79374030f745
hasan taweel <hasan.ali99976@gmail.com>
2026-09-14 15:27:13 +0300

    fix(pwa): force safe runtime rollout v124

--- app/pwa-register.tsx
+++ app/pwa-register.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect } from "react";
 
-const CURRENT_CACHE = "ostadh-lahooni-v123-stable";
-const SERVICE_WORKER_VERSION = "123-stable";
+const CURRENT_CACHE = "ostadh-lahooni-v124-stable";
+const SERVICE_WORKER_VERSION = "124-stable";
 
 export default function PwaRegister() {
   useEffect(() => {
