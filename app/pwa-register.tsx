726adbd172ad86e971fd9c93c8af8f9ebe0085ec
hasan taweel <hasan.ali99976@gmail.com>
2026-09-14 10:49:56 +0300

    fix(mobile): force fresh PWA worker v121

--- app/pwa-register.tsx
+++ app/pwa-register.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect } from "react";
 
-const CURRENT_CACHE = "ostadh-lahooni-v120-stable";
-const SERVICE_WORKER_VERSION = "120-stable";
+const CURRENT_CACHE = "ostadh-lahooni-v121-stable";
+const SERVICE_WORKER_VERSION = "121-stable";
 
 export default function PwaRegister() {
   useEffect(() => {
