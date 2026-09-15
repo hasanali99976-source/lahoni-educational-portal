6ffa275f23e41ccc13e1716ccb4ebf6e7f19ed89
hasan taweel <hasan.ali99976@gmail.com>
2026-09-16 00:01:14 +0300

    feat(v100): activate unified student portal design layer

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -1,5 +1,6 @@
 import "./student-classic-v103.css";
+import "../portal-v100.css";
 
 export default function StudentLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return <div className="portal-v100 student-app-shell" dir="rtl">{children}</div>;
 }
